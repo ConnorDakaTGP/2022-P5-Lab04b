bfa9810f61aa773a253a220bc44cc7443e069281
Connor Donegan <connor.donegan@vtsd.com>
2022-10-12 09:40:43 -0400

    Lab 04 B

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -29,7 +29,7 @@ public class Lab04bvst extends Applet
         g.drawLine(200,200,50,200);
         g.drawLine(50,200,125,275);
 
-        // DRAW SPHERE
+        //Main Circle
         g.drawOval(400,50,250,250);
         //Longitude
         g.drawOval(500,50,50,250);
@@ -44,17 +44,39 @@ public class Lab04bvst extends Applet
         g.drawOval(400,75,250,200);
         g.drawOval(400,50,250,250);
 
-        // DRAW INSCRIBED/CIRCUMSCRIBED TRIANGLE
+        //Outer ring
+
+        //Triangle
+
+        //Inner ring
 
 
+        //A
+        g.fillRect(25,475,25,125);
+        g.fillRect(75,475,25,125);
+        g.fillRect(50,475,25,25);
+        g.fillRect(50,525,25,25);
+        //P
+        g.fillRect(125,475,25,125);
+        g.fillRect(175,475,25,75);
+        g.fillRect(150,525,25,25);
+        g.fillRect(150,475,25,25);
+        //C
+        g.fillRect(225,475,25,125);
+        g.fillRect(250,475,50,25);
+        g.fillRect(250,575,50,25);
+        //S
+        g.fillRect(325,475,75,25);
+        g.fillRect(325,525,75,25);
+        g.fillRect(325,575,75,25);
+        g.fillRect(325,500,25,25);
+        g.fillRect(375,550,25,25);
 
-        // DRAW APCS
-
-
-
-        // DRAW PACMEN FLOWER
-
-
+        //pacmen flower
+        g.fillArc(600,375,75,75,-225,270);
+        g.fillArc(550,425,75,75,-135,270);
+        g.fillArc(600,475,75,75,-45,270);
+        g.fillArc(650,425,75,75,45,270);
 
     }
 
